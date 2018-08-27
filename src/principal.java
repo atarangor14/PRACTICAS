9c81aed1556eec3c55b40b79e3b8bd92f697a2e4
atarangor14 <42745373+atarangor14@users.noreply.github.com>
2018-08-27 09:42:08 -0600

    PRACTICA1
    FUNDAMENTOS

---
+++ src/principal.java
@@ -0,0 +1,22 @@
+/*
++ primera practica en java . definicion de nomenclatura para practicas
++ e introduccion al ambiente de desarrollo (IDE)
+ */
+
+/**
+ *
+ * @author Alejandro Tarango 18
+ */
+public class principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // primer linea de codigo: hola mundo !!
+        System.out.print("hola "); // sin salto de linea 
+        System.out.println("HOLA MUNDO !! ");//printl--> imprime un mensaje (salto de linea )
+
+    }
+    
+}
